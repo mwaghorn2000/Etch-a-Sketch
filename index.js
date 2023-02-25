ccf6419e5b6b9477e10bc21f9e3169ae4c59b239
mwaghorn2000 <m.waghorn2000@gmail.com>
2023-02-25 21:59:47 +1100

    added the ability to resize the grid with a button press

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-let container = document.getElementById("container");
-for (let i = 0; i < 16; i++) {
-  let gridDivRow = document.createElement("div");
-  gridDivRow.className = "grid-row";
-  gridDivRow.id = "grid-row";
-  for (let x = 0; x < 16; x++) {
-    let gridDiv = document.createElement("div");
-    gridDiv.className = "grid";
-    gridDiv.id = "grid";
-    gridDivRow.appendChild(gridDiv);
-  }
-  container.appendChild(gridDivRow);
-}
+const button = document.getElementById("reset");
 
-let grid = document.getElementsByClassName("grid");
-for (let j = 0; j < grid.length; j++) {
-  grid[j].addEventListener("mouseenter", (event) => {
-    event.target.style.backgroundColor = "black";
-  });
-}
+// adds click event to button which prompts user to pic a row size
+// for their etch-a-sketch
+button.addEventListener("click", () => {
+  let size = prompt("How many squares in each row?");
+  while (size < 1 || size > 100) {
+    size = prompt("Must be between 1 and 100");
+  }
+  resetGrid(size);
+});
+
+// creates an empty grid of divs. the parameter size detimines
+// the size of the row
+const resetGrid = (size) => {
+  let container = document.getElementById("container");
+  container.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    let gridDivRow = document.createElement("div");
+    gridDivRow.className = "grid-row";
+    for (let x = 0; x < size; x++) {
+      let gridDiv = document.createElement("div");
+      gridDiv.className = "grid";
+      gridDivRow.appendChild(gridDiv);
+    }
+    container.appendChild(gridDivRow);
+  }
+
+  // adds an event listener to all grid elements which changes the
+  // background color of the item hovered over to black
+  let grid = document.getElementsByClassName("grid");
+  for (let j = 0; j < grid.length; j++) {
+    grid[j].addEventListener("mouseenter", (event) => {
+      event.target.style.backgroundColor = "black";
+    });
+  }
+};
+
+resetGrid(16);
